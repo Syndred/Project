1454f20c4b1207214651c795735b76636d73ba81
Syndred <Syndred@163.com>
2023-06-25 21:30:10 +0800

    6252130

--- src/store/index.js
+++ src/store/index.js
@@ -4,46 +4,46 @@ import axios from "axios";
 export default createStore({
   state: {
     data: [
-      {
-        id: "0",
-        name: "小红",
-        age: "22",
-        eBG: "本科",
-        school: "广东技术师范大学",
-        wAge: "1",
-      },
-      {
-        id: "1",
-        name: "小蓝",
-        age: "23",
-        eBG: "本科",
-        school: "香港中文大学",
-        wAge: "1",
-      },
-      {
-        id: "2",
-        name: "小绿",
-        age: "33",
-        eBG: "研究生",
-        school: "深圳大学",
-        wAge: "4",
-      },
-      {
-        id: "3",
-        name: "小粉",
-        age: "32",
-        eBG: "研究生",
-        school: "厦门大学",
-        wAge: "5",
-      },
-      {
-        id: "4",
-        name: "紫薇",
-        age: "25",
-        eBG: "大专",
-        school: "深圳信息职业技术学院",
-        wAge: "1",
-      },
+      // {
+      //   id: "0",
+      //   name: "小红",
+      //   age: "22",
+      //   eBG: "本科",
+      //   school: "广东技术师范大学",
+      //   wAge: "1",
+      // },
+      // {
+      //   id: "1",
+      //   name: "小蓝",
+      //   age: "23",
+      //   eBG: "本科",
+      //   school: "香港中文大学",
+      //   wAge: "1",
+      // },
+      // {
+      //   id: "2",
+      //   name: "小绿",
+      //   age: "33",
+      //   eBG: "研究生",
+      //   school: "深圳大学",
+      //   wAge: "4",
+      // },
+      // {
+      //   id: "3",
+      //   name: "小粉",
+      //   age: "32",
+      //   eBG: "研究生",
+      //   school: "厦门大学",
+      //   wAge: "5",
+      // },
+      // {
+      //   id: "4",
+      //   name: "紫薇",
+      //   age: "25",
+      //   eBG: "大专",
+      //   school: "深圳信息职业技术学院",
+      //   wAge: "1",
+      // },
     ],
   },
   getters: {},
@@ -72,7 +72,7 @@ export default createStore({
     async fetchData({ commit }) {
       try {
         const response = await axios.get(
-          "http://192.168.1.105:8080/api/selectAll"
+          "http://192.168.43.202:8088/api/selectAll"
         );
         console.log(response.message);
         commit("Find", response.data);
@@ -87,7 +87,7 @@ export default createStore({
 
     del(context, id) {
       axios
-        .post(`http://192.168.1.105:8080/api/deleteOne/${id}`)
+        .post(`http://192.168.43.202:8088/api/deleteOne/${id}`)
         .then(function (response) {
           console.log(response);
         })
