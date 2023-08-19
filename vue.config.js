0a36c042130c01a883cb7e5206d1c951a1459163
Syndred <Syndred@163.com>
2023-08-19 18:58:23 +0800

    添加模拟数据

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     proxy: {
       "/api": {
         // target: "https://7648-113-100-10-5.ngrok-free.app",
-        // target: "http://192.168.33.207:8080",
+        target: "http://192.168.33.207:8080",
       },
     },
   },
